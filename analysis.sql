5da0f5be5d5487d195bf58bfa52dcd15c095f024
Bhavya346 <148934595+Bhavya346@users.noreply.github.com>
2026-04-29 20:46:07 +0530

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -12,3 +12,18 @@ select Sum(QTY) as total_quantity from amazon_sales;
 
 -- 5.
 select distinct Category from amazon_sales;
+
+-- 6.
+select Category,Sum(Amount) as revenue from amazon_sales group by Category order by Revenue desc;
+
+-- 7.
+select `ship-state`,count(*) as total_orders from amazon_sales group by `ship-state` order by total_orders desc limit 5 ;
+
+-- 8.
+select status,count(*) as total from amazon_sales group by Status order by total desc;
+
+-- 9.
+select Fulfilment,Sum(Amount) as Revenue from amazon_sales group by Fulfilment order by Revenue desc;
+
+-- 10.
+select avg(Amount) as Avg_order_value from amazon_sales;
