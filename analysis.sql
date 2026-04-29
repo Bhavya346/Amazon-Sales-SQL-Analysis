7977b5d80a3ae536b8aeff322898724c7e722ed5
Bhavya346 <148934595+Bhavya346@users.noreply.github.com>
2026-04-29 20:27:58 +0530

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -1,1 +1,14 @@
+-- 1.
+select * from amazon_sales limit 10;
 
+-- 2.
+select sum(Amount) as Total_Revenue from amazon_sales;
+
+-- 3.
+select count('Order_ID') as total_orders from amazon_sales;
+
+-- 4.
+select Sum(QTY) as total_quantity from amazon_sales;
+
+-- 5.
+select distinct Category from amazon_sales;
